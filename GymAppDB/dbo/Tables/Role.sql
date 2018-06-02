20a96d79061198f6ac91dbcd229059d218187236
Marlon <nickoo1987@gmail.com>
2018-06-02 18:22:13 -0300

    Uploading db initial structure

---
+++ GymAppDB/dbo/Tables/Role.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Role] (
+    [RoleID] INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED ([RoleID] ASC)
+);
+
---
+++ GymAppDB/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserID]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (50)  NOT NULL,
+    [LastName] NVARCHAR (100) NOT NULL,
+    [Age]      INT            NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
+
+
