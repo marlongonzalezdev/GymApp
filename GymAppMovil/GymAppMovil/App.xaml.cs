381517ebbf9a78229e5362146f911e6a2459ed95
nes0911 <nestornegrin@gmail.com>
2018-06-02 23:00:59 -0300

    Initials LoginPage

--- GymAppMovil/GymAppMovil/App.xaml.cs
+++ GymAppMovil/GymAppMovil/App.xaml.cs
@@ -1,3 +1,4 @@
+using GymAppMovil.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,26 +8,30 @@ namespace GymAppMovil
 {
 	public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new MainPage();
-		}
+            this.MainPage = new NavigationPage (new LoginPage());
+        }
+        #endregion
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
 }
---
+++ GymAppMovil/GymAppMovil/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymAppMovil.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
