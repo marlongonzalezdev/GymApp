4ba91f7b8be5d369e603163e97351c7ff7912f0f
Marlon <nickoo1987@gmail.com>
2018-06-08 00:52:09 -0300

    Uploading generic repository, also a test for it

--- GymAppRestService/Controllers/UserManagementController.cs
+++ GymAppRestService/Controllers/UserManagementController.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Persistence;
 
 namespace GymAppRestService.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UserManagementController : ApiController
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
         [HttpGet]
         [Route("GetUser")]
         public void GetUser(string user, string password)
         {
 
+            GymAppDBEntities context = new GymAppDBEntities();
+            IGenericRepository repo = new EntityFrameworkRepository<DbContext>(context);
+            IEnumerable<User> result = repo.GetAll<User>();
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Persistence/EntityFrameworkRepository.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public sealed class EntityFrameworkRepository<TContext> : IGenericRepository
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public EntityFrameworkRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        private IQueryable<TEntity> GetQueryable<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null,
+            int? skip = null,
+            int? take = null)
+            where TEntity : class
+        {
+            includeProperties = includeProperties ?? string.Empty;
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (skip.HasValue)
+            {
+                query = query.Skip(skip.Value);
+            }
+
+            if (take.HasValue)
+            {
+                query = query.Take(take.Value);
+            }
+
+            return query;
+        }
+
+        public IEnumerable<TEntity> GetAll<TEntity>(
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null,
+            int? skip = null,
+            int? take = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(null, orderBy, includeProperties, skip, take).ToList();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = null,
+        int? skip = null,
+        int? take = null)
+        where TEntity : class
+        {
+            return await GetQueryable<TEntity>(null, orderBy, includeProperties, skip, take).ToListAsync();
+        }
+
+        public IEnumerable<TEntity> Get<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null,
+            int? skip = null,
+            int? take = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter, orderBy, includeProperties, skip, take).ToList();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null,
+            int? skip = null,
+            int? take = null)
+            where TEntity : class
+        {
+            return await GetQueryable<TEntity>(filter, orderBy, includeProperties, skip, take).ToListAsync();
+        }
+
+        public TEntity GetOne<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            string includeProperties = "")
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter, null, includeProperties).SingleOrDefault();
+        }
+
+        public async Task<TEntity> GetOneAsync<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            string includeProperties = null)
+            where TEntity : class
+        {
+            return await GetQueryable<TEntity>(filter, null, includeProperties).SingleOrDefaultAsync();
+        }
+
+        public TEntity GetFirst<TEntity>(
+           Expression<Func<TEntity, bool>> filter = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           string includeProperties = "")
+           where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter, orderBy, includeProperties).FirstOrDefault();
+        }
+
+        public async Task<TEntity> GetFirstAsync<TEntity>(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null)
+            where TEntity : class
+        {
+            return await GetQueryable<TEntity>(filter, orderBy, includeProperties).FirstOrDefaultAsync();
+        }
+
+        public TEntity GetById<TEntity>(object id)
+            where TEntity : class
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public Task<TEntity> GetByIdAsync<TEntity>(object id)
+            where TEntity : class
+        {
+            return _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public int GetCount<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter).Count();
+        }
+
+        public Task<int> GetCountAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter).CountAsync();
+        }
+
+        public bool GetExists<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter).Any();
+        }
+
+        public Task<bool> GetExistsAsync<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+            where TEntity : class
+        {
+            return GetQueryable<TEntity>(filter).AnyAsync();
+        }
+
+        public void Create<TEntity>(TEntity entity, string createdBy = null)
+        where TEntity : class
+        {
+            //entity.CreatedDate = DateTime.UtcNow;
+            //entity.CreatedBy = createdBy;
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public void Update<TEntity>(TEntity entity, string modifiedBy = null)
+            where TEntity : class
+        {
+            //entity.ModifiedDate = DateTime.UtcNow;
+            //entity.ModifiedBy = modifiedBy;
+            _context.Set<TEntity>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete<TEntity>(object id)
+            where TEntity : class
+        {
+            TEntity entity = _context.Set<TEntity>().Find(id);
+            Delete(entity);
+        }
+
+        public void Delete<TEntity>(TEntity entity)
+            where TEntity : class
+        {
+            var dbSet = _context.Set<TEntity>();
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                ThrowEnhancedValidationException(e);
+            }
+        }
+
+        public Task SaveAsync()
+        {
+            try
+            {
+                return _context.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException e)
+            {
+                ThrowEnhancedValidationException(e);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private void ThrowEnhancedValidationException(DbEntityValidationException e)
+        {
+            var errorMessages = e.EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+
+            var fullErrorMessage = string.Join("; ", errorMessages);
+            var exceptionMessage = string.Concat(e.Message, " The validation errors are: ", fullErrorMessage);
+            throw new DbEntityValidationException(exceptionMessage, e.EntityValidationErrors);
+        }
+    }
+}
