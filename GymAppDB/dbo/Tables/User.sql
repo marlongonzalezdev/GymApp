787fc5578254426577b6222f6eebb9f28639bd17
Marlon <nickoo1987@gmail.com>
2018-06-08 00:57:37 -0300

    Upload db changes

--- GymAppDB/dbo/Tables/User.sql
+++ GymAppDB/dbo/Tables/User.sql
@@ -1,10 +1,20 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [UserID]   INT            IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (50)  NOT NULL,
-    [LastName] NVARCHAR (100) NOT NULL,
-    [Age]      INT            NOT NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
+    [UserID]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50)  NOT NULL,
+    [LastName]     NVARCHAR (100) NOT NULL,
+    [Password]     NVARCHAR (MAX) NOT NULL,
+    [Nickname]     NVARCHAR (50)  NOT NULL,
+    [Age]          INT            NOT NULL,
+    [CreatedDate]  DATETIME       NULL,
+    [ModifiedDate] DATETIME       NULL,
+    [CreatedBy]    DATETIME       NULL,
+    [ModifiedBy]   DATETIME       NULL,
+    [RoleID]       INT            NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC),
+    CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Role] ([RoleID])
 );
+
+
 
 
 
