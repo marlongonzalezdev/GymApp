49fe6cba83a7e0aaaf401c8d612e9faa16783dc6
nes0911 <nestornegrin@gmail.com>
2018-06-12 18:08:38 -0300

    Initials HomePage

---
+++ GymAppMovil/GymAppMovil/ViewModels/LoginViewModel.cs
@@ -0,0 +1,120 @@
+﻿namespace GymAppMovil.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Views;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
+    {        
+
+        #region Attributes
+        private string user;
+        private string password;
+        private bool isRemember;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string User
+        {
+            get { return this.user; }
+            set { SetValue(ref this.user, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRemember
+        {
+            get { return this.isRemember; }
+            set { SetValue(ref this.isRemember, value); }
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new RelayCommand(Register);
+            }
+        }        
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+            this.IsEnabled = true;
+        }        
+        #endregion
+
+        #region Methods
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.User))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar un Usuario...",
+                    "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar un Password...",
+                    "Aceptar");
+                return;
+            }
+
+            this.IsEnabled = false;
+
+            if (this.User != "nestor" || this.Password !="1234")
+            {
+                this.IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Usuario ó Password incorrecto...",
+                    "Aceptar");
+
+                this.Password = string.Empty;
+
+                return;
+            }
+
+            this.IsEnabled = true;
+            this.User = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Home = new HomeViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new HomePage());
+        }
+
+        private void Register()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
--- GymAppMovil/GymAppMovil/ViewModels/MainViewModel.cs
+++ GymAppMovil/GymAppMovil/ViewModels/MainViewModel.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GymAppMovil.ViewModels
+﻿namespace GymAppMovil.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        public HomeViewModel Home { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+
+            return instance;
+        }
+        #endregion
     }
 }
